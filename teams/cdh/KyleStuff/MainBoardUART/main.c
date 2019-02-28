a42f15de461ea89a252ef1d007c72a8a00c90980
Kyle Shaff <kylets@uw.edu>
2019-02-27 17:13:08 -0800

    Cleaned up the code, contains base UART code with a working sample (External loopback)

--- teams/cdh/KyleStuff/MainBoardUART/main.c
+++ teams/cdh/KyleStuff/MainBoardUART/main.c
@@ -12,7 +12,7 @@ void initUART(void){
     UCA3BR1 = 0;
     UCA3MCTLW = UCBRS1;
 
-    //UCA3STATW |=  UCLISTEN; //Loopback mode
+    //UCA3STATW |=  UCLISTEN; //Internal loopback mode
 
     //enable the USCI peripheral
     UCA3CTLW0 &= ~UCSWRST;
@@ -20,6 +20,23 @@ void initUART(void){
     UCA3IE |= (UCTXIE|UCRXIE);
 }
 
+void putChar(char c){
+    //wait for the buffer to be ready
+    while(!(UCA3IFG & UCTXIFG));
+
+    //transmit data
+    UCA3TXBUF = c;
+    __bis_SR_register(LPM0_bits | GIE); // Enter LPM0, interrupts enabled
+}
+
+int getChar(void){
+    int chr = -1;
+
+    if(UCA3IFG & UCRXIFG){
+        chr = UCRXBUF3;
+    }
+    return chr;
+}
 
 /**
  * main.c
@@ -41,65 +58,11 @@ int main(void){
     CSCTL2 = SELS__DCOCLK;
 
     initUART();
-
-    while (1)
-        {
-            //while(!(UCA3IFG & UCTXIFG));
-
-            UCA3TXBUF = 'A';                 // Load data onto buffer
-
-            __delay_cycles(1000);
-            __bis_SR_register(LPM0_bits | GIE); // Enter LPM0, interrupts enabled
-
-        }
-
+    while(1){
+        putChar('A');
+    }
     return 0;
 }
-/*
-void initUART(void){
-    //9600 8N1
-    UCA3CTLW0 |= UCSWRST;//has to be set to initialize board
-    //sets DCO to 1mHz
-    CSCTL1 |= DCOFSEL_0;
-    //sets SMCLK to use DCO
-    CSCTL2 |= SELS_3;
-    //set ports for UART
-    P6SEL1 &= ~(BIT0 | BIT1);
-    P6SEL0 |= (BIT0 | BIT1);
-    //set clock source to SMCLK
-    UCA3CTLW0 |= UCSSEL_2;
-    //set baud rate using table from data sheet
-    UCA3BR0 = 104;
-    UCA3BR1 = 0;
-    UCA3MCTLW = UCBRS1;
-    //enables interrupts
-    UCA3IE |= (UCTXIE|UCRXIE);
-
-    UCA3STATW |=  UCLISTEN; //Loopback mode
-
-    //enable the USCI peripheral
-    UCA3CTLW0 &= ~UCSWRST;
-
-
-
-}
-
-int getChar(void){
-    int chr = -1;
-
-    if(UCA3IFG & UCRXIFG){
-        chr = UCRXBUF3;
-    }
-    return chr;
-}
-
-void putChar(int c){
-    //wait for the buffer to be ready
-    while(!(UCA3IFG & UCTXIFG));
-
-    //transmit data
-    UCA3TXBUF = (char)c;
-}*/
 
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector = EUSCI_A3_VECTOR             // eUSCI ISR
@@ -118,15 +81,12 @@ void __attribute__ ((interrupt(EUSCI_A3_VECTOR))) USCI_A3_ISR(void)
          P1OUT |= (0x01);
 
          UCA3IFG &= ~UCRXIFG;
-
          __bic_SR_register_on_exit(LPM0_bits);
 
          break; // Vector 2: UCRXIFG
       case USCI_UART_UCTXIFG:
           P1OUT |= (0x02);
           //__delay_cycles(10000);
-          P1OUT &= ~(BIT0|BIT1);
-
          break; // Vector 4: UCTXIFG
       case USCI_UART_UCSTTIFG: break;
       case USCI_UART_UCTXCPTIFG: break;
