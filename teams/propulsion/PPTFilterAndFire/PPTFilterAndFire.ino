50dca4cf629e0f50a4fa0cb188cc9e4526cb32fb
ManoRit18 <32560046+ManoRit18@users.noreply.github.com>
2019-05-21 19:15:06 -0700

    Combined Code
    Combined SD, probe arm, filtering, and Charge/Fire PPT functionality. This is how each component should work together. I still need to test debouncingInput() and the sd card writing code. Additionally, I am unsure about when exactly the probe arm should retract() since I don't think there's a way to tell when the PPT will stop firing for good.

--- teams/propulsion/PPTFilterAndFire/PPTFilterAndFire.ino
+++ teams/propulsion/PPTFilterAndFire/PPTFilterAndFire.ino
@@ -1,143 +1,223 @@
-// Note to self: 1st try debouncing, try to make test noise more specific to needs
+#include <SD.h>
+#include <SPI.h>
+#include <Stepper.h>
 
-// Filtering signal variables
-int signalPin = 2;
-int lastButtonState = LOW;
-int buttonState;
-int buttonPressed = false;
+// Writing to SD Card Variables & Constants
+int chipSelect = 10;
+
+// Filtering Signal Variables & Constants
+int readPin = 3;
+int lastSignalState = LOW;
+int signalState = LOW;
+//int signalHigh = false;
 float voltage;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 200;
+boolean signalTest = LOW;
 
-// PatternGenerator variables
+// Test Signal Generator Variables & Constants
+int signalPin = 2;
 unsigned long pattern;
 unsigned long timeShiftPrev;
 byte shiftCount = 0, signalSelect;
+int intervalCount = 0;
+int numberOfBitsOfSignal = 32;
+byte experimentalRightConditionSignal = 4;
 
-// charge and fire ppt variables and constants
-int firePin = 5;
-int commandPin = 6;
-int chargePin = 8;
+// Charge and Fire PPT Variables and Constants
+int firePin = 7;
+int chargePin = 8; 
+float maxVoltageThreshold = 2.5;
+double tempFireSpacingInterval;
+double fireSpacingInterval = 1000;
+double startFireTime = 0;
+double lastFireTime = 0;
+double fireInterval = 5000;
+boolean startingFire = true;
+boolean PPTFiringNow = false;
 
-//reading analog, voltage (from feedback)
+// Reading Analog Pins - Voltages Constants & Variables
 int feedbackPin = A0;
-int groundReferencePin = A2;
+int groundReferencePin = A1;
 
-// Not sure about min voltage, chose 1
-float minVoltageThreshold = 1.973;
-float maxVoltageThreshold = 2.467;
-// Put in dummy values for r1 and r2 because I wasn't sure about them
-boolean fireCommand;
+// Probe Arm Constants & Variables
+const int in_1 = 9;
+const int in_2 = 10;
+const int in_3 = 11;
+const int in_4 = 12;
+const int steps_per_revolution = 64; // nominal steps per revlution
+const int reduction_ratio = 32;
+const int motor_speed = 8; // rpm
+int real_steps_per_revolution = steps_per_revolution*reduction_ratio;
+Stepper s(real_steps_per_revolution, in_1, in_2, in_3, in_4);
+boolean PPTJustStartedFiring = true;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(signalPin, INPUT);
+  pinMode(signalPin, OUTPUT);
+  pinMode(readPin, INPUT);
   pinMode(feedbackPin, INPUT);
   pinMode(groundReferencePin, INPUT);
   pinMode(firePin, OUTPUT);
-  pinMode(chargePin, OUTPUT);
-  pinMode(commandPin, OUTPUT);
-  //set fire command initially to false
-  fireCommand=false;
-  voltage = 0.0;
-}
-/* The main computer gives a fireCommand (high/low). This code does nothing
- *  until the main computer gives the command to fire. Once this happens, it 
- *  begins to charge the main capacitor for 4 seconds, reads the value, and 
- *  continues to charge until max threshold is met. It will fire than than
- *  measure the voltage. If it's 0 than it was a successfull fire.
- * 
- */
-void loop() {
-  // put your main code here, to run repeatedly:
-  //patternGenerator(); 
-  //charge and fire code in here:
-  while(fireCommand){
-     chargeToMax();
-     digitalWrite(firePin, HIGH); //firePPT
-     digitalWrite(firePin, LOW);
-     measureVoltage();  //refresh voltage post fire
+  pinMode(chargePin, OUTPUT);  
+  
+  if (!SD.begin(chipSelect)) {
+    //Serial.println("Card failed, or not present");
   }
-  debouncingInput();
+  s.setSpeed(motor_speed);
 }
 
-// Checks if input has been stable for a certain number of seconds, 
-// which would then reveal that it isn't noise
-void debouncingInput(){
-  if(buttonPressed){
-    //chargeAndFirePPT();
+void loop() {
+  //// Methods need to run at same time.
+  //// debouncing Input is an
+  //// extra redundant software
+  //// signal filter that will ensure
+  //// the true state of the signal is 
+  //// not changed by noise.
+  ///////////////////////////////  
+  
+  if(millis() > 0){
+    //debouncingInput();
   }
-  else{
-    int reading = digitalRead(signalPin);
-    if(reading != lastButtonState){
-      lastDebounceTime = micros();
+  
+  if(millis() > 0 && signalState){
+    chargeAndFirePPT();
+  }
+
+  // As soon as the PPT starts firing (signalState == HIGH) and a
+  // plasma stream is created, the probe arm should extend and 
+  // start taking temperatures and writing to the SD card.
+  if(millis() > 0 && signalState){
+    if(PPTJustStartedFiring){
+      extend();
+      PPTJustStartedFiring = false;
     }
-    if((micros() - lastDebounceTime) > debounceDelay){
-      if(reading != buttonState){
-        buttonState = reading;
-        if(buttonState){
-          buttonPressed = true;
-        }
+    if(PPTFiringNow){
+      writingTempDataToSD();
+    }
+  }
+  if(signalState == LOW){
+    PPTJustStartedFiring = true;
+    // This temporary variable is needed so that the PPT fires right away the 
+    // first time it starts firing in its charge-fire cycle (instead of putting
+    // a space interval at the beggining)
+    tempFireSpacingInterval = 0;
+    retract();
+  }
+}
+  
+void chargeAndFirePPT(){
+    if(measureVoltage() <= maxVoltageThreshold){
+      digitalWrite(firePin, LOW);
+      digitalWrite(chargePin, HIGH);
+      PPTFiringNow = false;
+    }
+    
+    //If the fire spacing interval has been reached ...
+    else if(millis() - lastFireTime > tempFireSpacingInterval){
+      if(startingFire){
+        startingFire = false;
+        startFireTime = millis(); 
+      }
+      digitalWrite(chargePin, LOW);
+      digitalWrite(firePin, HIGH);
+      PPTFiringNow = true;
+      // ... fire PPT for specified fire interval and
+      // reset fire spacing interval clock to 
+      // zero after PPT is done firing
+      if(millis() - startFireTime > fireInterval){
+        lastFireTime = millis();
+        tempFireSpacingInterval = fireSpacingInterval;
+        startingFire = true;
       }
     }
-    lastButtonState = reading;
-  }
+    else{
+      digitalWrite(firePin, LOW);
+      digitalWrite(chargePin, LOW);
+      PPTFiringNow = false;
+    }
 }
 
-/*  Measure the voltage of the main capacitor over the voltage divider 
- *   through analog input 0.
- *   0.329 V = 100 V
- *         ...
- *   1.645 V = 500 V
- *         ...
- *   2.467 V = 750 V
- */
-void measureVoltage(){
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(feedbackPin) - analogRead(groundReferencePin);
+// feedback telling PPT when it needs to start charging
+double measureVoltage(){
+  int sensorValue = abs(analogRead(feedbackPin) - analogRead(groundReferencePin));
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   double voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
-  
+  //Serial.print("VOLTAGE ");
+  //Serial.println(voltage);
+  return voltage; 
 }
 
+void writingTempDataToSD(){
+  String dataString = "("; 
+  dataString += micros(); dataString += ","; dataString += "temp";
+  dataString += ")";
 
-/* Charge the circuit for four seconds than measure until 
- *  max voltage threshold is reached. * 
- */
-void chargeToMax(){
-  while(voltage <= maxVoltageThreshold){
-    digitalWrite(chargePin, HIGH); //begin charging
-    delay(4000); //delay for four seconds
-    digitalWrite(chargePin, LOW); //stop charging
-    measureVoltage(); //refresh voltage
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
   }
 }
 
+void extend() {
+  s.step(100*reduction_ratio);
+}
 
-// Creates a signal with some noise, you can ignore this code, it was 
-// meant for testing
-void patternGenerator(){
-  const unsigned long normallyLowSignal = 0x000AF500;
-  const unsigned long normallyHighSignal = 0xFFF50AFF;
-  const unsigned long shiftInterval = 10000; //100
+void retract() {
+  s.step(-100*reduction_ratio);
+}
 
+// Method for Filtering Signal
+void debouncingInput(){
+  int reading = digitalRead(readPin);
+ 
+  // if consective readings don't match up, then start a debouncing period
+  // at that point so we can wait it out and see if its noise or a signal change
+  if(reading != lastSignalState){
+    lastDebounceTime = micros();
+  }
+  // If its been 200 microseconds, the length of noise 
+  if((micros() - lastDebounceTime) > debounceDelay){
+    if(reading != signalState){
+      signalState = reading;
+    }
+  }
+  lastSignalState = reading;
+  //Serial.println(signalState);
+}
+
+
+// Describes the test noise signal used to test debouncingInput(), testing still needs to be done
+// Will not be present in code deployed to Arduino upon flight (replaced by actual signal from main comp)
+void testSignalGenerator() {
+  // Noise lasting about 196 milliseconds and dipping low briefly at ends, should still be registered 
+  // as "HIGH" by debouncing input                       
+  const unsigned long noisySignal =        0b00111111111111111111111111111100;  
+  // 224 milliseconds high signal, should be registered as "HIGH" for debouncing input                           
+  const unsigned long normallyHighSignal = 0b11111111111111111111111111111111;
+  const unsigned long shiftInterval = 7; //3.125
+
+  // Should all scale down to micros for final product, millis() easier to see for testing
   unsigned long timeShiftNow = micros();
+
   if (timeShiftNow - timeShiftPrev > shiftInterval)
   {
     timeShiftPrev += shiftInterval;
     pattern = pattern >> 1;
     shiftCount++;
-    if (shiftCount >= 32)
-    {
+    
+    if(shiftCount >= numberOfBitsOfSignal){
       shiftCount = 0;
       signalSelect++;
-      (signalSelect & 5000) ? pattern = normallyHighSignal : pattern = normallyLowSignal;
+      (signalSelect & experimentalRightConditionSignal) ? pattern = noisySignal : pattern = normallyHighSignal;
     }
     digitalWrite(signalPin, pattern & 1);
-    int val = digitalRead(signalPin);
-    Serial.print(val);
+    signalState = digitalRead(readPin);
+    //Serial.println(signalState);
  }
 }
